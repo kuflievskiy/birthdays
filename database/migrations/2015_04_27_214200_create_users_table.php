e20f784d28ee563d0c24e580702710fa7fdf5d4d
kuflievskiy <kuflievskiy@gmail.com>
2017-04-19 23:38:14 +0300

    Issue #11 - attempt to fix , removed duplicated migration class CreateUsersTable

--- database/migrations/2015_04_27_214200_create_users_table.php
+++ database/migrations/2015_04_27_214200_create_users_table.php
@@ -12,12 +12,16 @@ class CreateUsersTable extends Migration {
 	 */
 	public function up()
 	{
-		Schema::create('users', function(Blueprint $table)
-		{
-			$table->increments('id');
-			$table->string('email')->unique();			
-			$table->timestamps();
-		});
+		if (!Schema::hasTable('users')) {
+			Schema::create('users', function (Blueprint $table) {
+				$table->increments('id');
+				$table->string('name');
+				$table->string('email')->unique();
+				$table->string('password');
+				$table->rememberToken();
+				$table->timestamps();
+			});
+		}
 	}
 
 	/**
