ff5e79d7c74baf154c909c918cb80c16992d4fb8
kuflievskiy <kuflievskiy@gmail.com>
2017-05-23 18:39:48 +0300

    Issue #11 - worked on codeception tests

--- database/migrations/2015_04_27_214200_create_users_table.php
+++ database/migrations/2015_04_27_214200_create_users_table.php
@@ -15,7 +15,6 @@ class CreateUsersTable extends Migration {
 		if (!Schema::hasTable('users')) {
 			Schema::create('users', function (Blueprint $table) {
 				$table->increments('id');
-				$table->string('name');
 				$table->string('email')->unique();
 				$table->string('password');
 				$table->rememberToken();
