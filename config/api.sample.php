aad98a15b01a131d9f63f4a2e902b443570c3a09
kuflievskiy <kuflievskiy@gmail.com>
2017-05-26 10:27:37 +0300

    rest api test

--- config/api.sample.php
+++ config/api.sample.php
@@ -2,6 +2,7 @@
 // is used in app/Helpers/APIConnection.php
 return [
 	'clients' => [
+        '10.10.0.1'=>'Codeception',
 		//'11.11.11.11' => 'FoJB+Du8dG:;p|y[w`#lCElU9J(1ri+|@8SVC|obmSbl5+od+t>7oDdU9QwnWtr`'
 	]
 ];
